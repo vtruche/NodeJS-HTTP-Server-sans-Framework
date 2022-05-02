8004e6db2b5897427a071bad319a3c177cb3c75e
Vincent Truche <vince.truche@gmail.com>
2022-05-02 15:44:43 +0200

    Question 2 : Ajout du status code

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	res.write('<h1>Hello World</h1>');
+	res.writeHead(200, {'content-type':'text/html'});
+	res.write('<h1>Hello World Vincent !</h1>');
 	res.end();
 });
 
