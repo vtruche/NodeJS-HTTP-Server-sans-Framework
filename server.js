ecc13b9ca696803b795585a9db48ed849c12f4db
Vincent Truche <vince.truche@gmail.com>
2022-05-02 16:10:53 +0200

    Question 5 : Ajout d'un Try/Catch pour éviter les erreurs internes au serveur

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	if (req.url === "/") {
-		res.writeHead(200, {'content-type':'text/html'});
-		res.write('<h1>Hello World Vincent !</h1>');
-		res.end();
-		if (req.method !=== "GET") {
-			res.writeHead(403, {'content-type':'text/html'});
-			res.write('<h1>403 Méthode non authorisée</h1>');
+	try {
+		if (req.url === "/") {
+			res.writeHead(200, {'content-type':'text/html'});
+			res.write('<h1>Hello World Vincent !</h1>');
+			res.end();
+			if (req.method !=== "GET") {
+				res.writeHead(403, {'content-type':'text/html'});
+				res.write('<h1>403 Méthode non authorisée</h1>');
+				res.end();
+			}
+		}
+		else {
+			res.writeHead(404, {'content-type':'text/html'});
+			res.write('<h1>404 Page Introuvable</h1>');
 			res.end();
 		}
 	}
-	else {
-		res.writeHead(404, {'content-type':'text/html'});
-		res.write('<h1>404 Page Introuvable</h1>');
+	catch (err) {
 		res.end();
 	}
 });
