b2ceae58f25f14eb2b58b5beaa902600dd76f6a4
Vincent Truche <vince.truche@gmail.com>
2022-05-02 15:45:47 +0200

    Question 2 : Ajout de la route /

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-	res.writeHead(200, {'content-type':'text/html'});
-	res.write('<h1>Hello World Vincent !</h1>');
-	res.end();
+	if (req.url === "/") {
+		res.writeHead(200, {'content-type':'text/html'});
+		res.write('<h1>Hello World Vincent !</h1>');
+		res.end();
+	}
 });
 
 server.listen(5000);
