425cf79ab4b46204d1f88925dea8befe2927e462
Vincent Truche <vince.truche@gmail.com>
2022-05-05 09:42:53 +0200

    Remise au propre du projet avec le code du prof

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const http = require('http');
 
+const fs = require('fs');
+const path = require('path');
+
+let contenuHtml;
+let cheminAbsolu;
+
 const server = http.createServer((req, res) => {
 	try {
 		if (req.url === "/") {
 			res.writeHead(200, {'content-type':'text/html'});
-			res.write('<h1>Hello World Vincent !</h1>');
+			// res.write('<h1>Hello World Vincent !');
+			cheminAbsolu = path.join(__dirname , 'public/pages/index.html');
+			contenuHtml = fs.readFilesSync(cheminAbsolu, "utf-8");
+			res.write(contenuHtml);
 			res.end();
-			if (req.method !=== "GET") {
+			if (req.method !== "GET") {
 				res.writeHead(403, {'content-type':'text/html'});
 				res.write('<h1>403 Méthode non authorisée</h1>');
 				res.end();
